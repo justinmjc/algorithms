296d90fc2820d8e576f4121d37092459a59ff281
root <justin@colaedu.com>
2021-06-04 09:52:27 +0800

    MoveZeroes

--- array_list_skiplist/leetcode/move_zeroes_test.go
+++
@@ -1,8 +0,0 @@
-package leetcode
-
-import "testing"
-
-func TestMoveZeroes1(t *testing.T) {
-	arr := []int{1, 2, 0, 1, 3, 0, 2, 4, 0}
-	MoveZeroes1(arr)
-}
--- main.go
+++ main.go
@@ -4,8 +4,5 @@ import "fmt"
 
 func main() {
 
-	arr := []int{1, 2, 3, 4, 5, 6}
-	arr = append(arr[:2], arr[3:]...)
-	fmt.Println(arr)
-
+	fmt.Println(15 / 10)
 }
